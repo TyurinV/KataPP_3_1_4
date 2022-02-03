baa3c937f2c7d45f5f1d6fe6b431ab07141030ed
Vadim T <denfal@mail.ru>
2022-02-03 12:46:57 +0300

    sql add for start

---
+++ src/main/resources/data.sql
@@ -0,0 +1,14 @@
+INSERT INTO users.users (firstName, lastName, password, sex, smoker)
+VALUES ('admin', 'admin', '$2a$10$EtEWiG1tGWS1asBmswEBr.77djC2N63eyowDfjRbh4Vl1UUKTeF3C', 'M', true);
+
+INSERT INTO users.roles (role_name)
+VALUES ('ROLE_ADMIN');
+
+INSERT INTO users.roles (role_name)
+VALUES ('ROLE_USER');
+
+INSERT INTO users.user_role (user_id, role_id)
+VALUES (1, 1);
+
+INSERT INTO users.user_role (user_id, role_id)
+VALUES (1, 2);
